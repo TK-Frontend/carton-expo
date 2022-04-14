c341deccb9612013069560797d323236672c65ed
unknown <teqa@wp.pl>
2022-04-14 11:24:20 +0200

    Added store, provider to use redux

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { View } from 'react-native';
-import Screens from './src/navigation';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./src/app/store";
+import { SafeAreaView } from "react-native";
+import Screens from "./src/navigation";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <Screens />
-    </View>
+    <Provider store={store}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Screens />
+      </SafeAreaView>
+    </Provider>
   );
 }
