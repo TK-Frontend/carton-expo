e6304d6a7f1256cd76dfa536d0e4a0db694a5e01
unknown <teqa@wp.pl>
2022-04-14 11:25:08 +0200

    Added new color

--- src/constans/colors.ts
+++ src/constans/colors.ts
@@ -2,9 +2,12 @@ export interface ColorValue {
   BLACK: string;
   BLUE: string;
   GOLD: string;
+  GREEN: string;
   GREY: string;
+  RED: string;
   WHITE: string;
   GRADIENT_BLUE: Array<string>;
+  GRADIENT_LIGHT_BLUE: Array<string>;
   GRADIENT_ORANGE: Array<string>;
   GRADIENT_BROWN: Array<string>;
   GRADIENT_BROWN2: Array<string>;
@@ -15,9 +18,12 @@ export const Colors: ColorValue = {
   BLACK: "#000000",
   BLUE: "#2BA8E0",
   GOLD: "#F7B900",
+  GREEN: "#00C300",
   GREY: "#B0B2B4",
+  RED: "#CF0000",
   WHITE: "#ffffff",
-  GRADIENT_BLUE: ["#20457d", "#182a5f", "#181a4b"],
+  GRADIENT_BLUE: ["#20457D", "#182A5F", "#181A4B"],
+  GRADIENT_LIGHT_BLUE: ["#22C3F3", "#4169B2"],
   GRADIENT_ORANGE: ["#C79E79", "#C18F6C"],
   GRADIENT_BROWN: ["#D8A069", "#e6b980"],
   GRADIENT_BROWN2: ["#e6b980", "#eacda3"],
