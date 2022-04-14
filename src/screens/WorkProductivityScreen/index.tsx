40319c41cef77420aa3466f01efd6df2cee82473
unknown <teqa@wp.pl>
2022-04-14 11:27:13 +0200

    Changes

--- src/screens/WorkProductivityScreen/index.tsx
+++ src/screens/WorkProductivityScreen/index.tsx
@@ -1,18 +1,101 @@
-import React, {useMemo} from 'react';
-import {View, Text} from 'react-native';
-import {NavigationProp, ParamListBase} from '@react-navigation/native';
-import createStyles from './styles';
+import React, { useMemo, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
+import createStyles from "./styles";
+import { LinearGradient } from "expo-linear-gradient";
+import { Colors } from "../../constans/colors";
 
-interface IProps {
+interface Props {
   navigation: NavigationProp<ParamListBase>;
 }
 
-const WorkProductivityScreen: React.FC<IProps> = ({navigation}) => {
+const WorkProductivityScreen: React.FC<Props> = ({ navigation }) => {
   const styles = useMemo(() => createStyles(), []);
+
+  const [productivity, setProductivity] = useState("256 k/h");
+  const [cartonsDone, setCartonsDone] = useState([
+    125, 456, 12, 120, 250, 48, 173, 555, 666, 777, 888, 999, 1000, 2000, 3000,
+  ]);
+
+  const sum = cartonsDone.reduce(function (prev, curr) {
+    return prev + curr;
+  });
+
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>WorkProductivityScreen</Text>
-    </View>
+    <LinearGradient colors={Colors.GRADIENT_BLUE} style={styles.box}>
+      <View style={styles.container}>
+        <View style={styles.wrapper}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Skompletowane zlecenia:</Text>
+            <Text style={styles.gold}>{cartonsDone.length}</Text>
+          </View>
+
+          <View style={styles.completed}>
+            {cartonsDone.map((number, index) => (
+              <Text key={index} style={styles.completedText}>
+                {number}
+              </Text>
+            ))}
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Suma:</Text>
+            <Text style={styles.gold}>{sum}</Text>
+          </View>
+          {/* <View style={styles.line}></View> */}
+        </View>
+
+        <View style={styles.wrapper}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Godzina rozpoczęcia:</Text>
+            <Text style={styles.green}>14:00</Text>
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Czas pracy:</Text>
+            <Text style={styles.green}>4h 41m 20s</Text>
+          </View>
+          {/* <View style={styles.line}></View> */}
+        </View>
+
+        <View style={styles.wrapper}>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Wydajność</Text>
+            <Text style={styles.red}>{productivity}</Text>
+          </View>
+        </View>
+
+        <View style={[styles.wrapper]}>
+          <View>
+            <TextInput
+              placeholder="Podaj ilość"
+              keyboardType="numeric"
+              placeholderTextColor={Colors.GREY}
+              // value={email}
+              // onChangeText={(text) => setEmail(text)}
+              style={styles.input}
+            />
+          </View>
+
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              style={[styles.button, styles.buttonHalf]}
+              // onPress={() => {handleSignIn()}}
+            >
+              <Text style={styles.buttonText}>Cofnij</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={[styles.button, styles.buttonHalf]}>
+              <Text style={styles.buttonText}>Dodaj</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={[styles.button, styles.buttonBig]}>
+              <Text style={styles.buttonText}>Zakończ</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </LinearGradient>
   );
 };
 
--- src/screens/WorkProductivityScreen/styles.ts
+++ src/screens/WorkProductivityScreen/styles.ts
@@ -1,18 +1,126 @@
-import {StyleSheet} from 'react-native';
-import {RFValue} from 'react-native-responsive-fontsize';
-import {Colors} from '../../constans/colors';
+import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Colors } from "../../constans/colors";
 
 const createStyles = () =>
   StyleSheet.create({
     body: {
+      alignItems: "center",
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: "center",
+    },
+    borderDashed: {
+      alignItems: "center",
+      borderColor: Colors.BLUE,
+      borderStyle: "dashed",
+      borderWidth: 3,
+      borderRadius: RFValue(8),
+      padding: RFValue(10),
+      width: "88%",
+    },
+    box: {
+      flex: 1,
+    },
+    button: {
+      backgroundColor: Colors.BLUE,
+      borderRadius: RFValue(8),
+      color: Colors.WHITE,
+      marginVertical: RFValue(6),
+      padding: RFValue(10),
+    },
+    buttonBig: {
+      width: "100%",
+      paddingVertical: RFValue(16),
+    },
+    buttonsContainer: {
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-between",
+    },
+    buttonHalf: { flexBasis: "48%", paddingVertical: RFValue(16) },
+    buttonText: {
+      color: Colors.WHITE,
+      textAlign: "center",
+      fontSize: RFValue(16),
+      fontWeight: "700",
+    },
+    completed: {
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      height: RFValue(128),
+      justifyContent: "flex-start",
+      marginVertical: RFValue(8),
+    },
+    completedText: {
+      color: Colors.GREY,
+      fontSize: RFValue(24),
+      marginRight: RFValue(8),
+    },
+    container: {
+      alignItems: "center",
+      flex: 1,
+      justifyContent: "center",
+    },
+    green: {
+      color: Colors.GREEN,
+      fontSize: RFValue(22),
+      marginLeft: RFValue(8),
+    },
+    gold: {
+      color: Colors.GOLD,
+      fontSize: RFValue(22),
+      marginLeft: RFValue(8),
+    },
+    input: {
+      borderColor: Colors.WHITE,
+      borderWidth: RFValue(1),
+      borderRadius: RFValue(8),
+      color: Colors.WHITE,
+      fontSize: RFValue(22),
+      marginVertical: RFValue(8),
+      paddingHorizontal: RFValue(96),
+      paddingVertical: RFValue(16),
+      textAlign: "center",
+    },
+    line: {
+      borderWidth: 1,
+      borderColor: Colors.WHITE,
+      marginVertical: RFValue(16),
+      width: "100%",
+    },
+    productivityText: {
+      alignItems: "center",
+    },
+    red: {
+      color: Colors.RED,
+      fontSize: RFValue(22),
+      marginLeft: RFValue(8),
     },
     text: {
-      fontSize: RFValue(25),
-      fontWeight: '300',
-      color: Colors.BLACK,
+      alignContent: "flex-start",
+      color: Colors.WHITE,
+      fontSize: RFValue(22),
+    },
+    textContainer: {
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "row",
+    },
+    textUpper: {
+      textTransform: "uppercase",
+    },
+
+    wrapper: {
+      borderColor: Colors.GREY,
+      borderRadius: RFValue(8),
+      borderWidth: RFValue(1),
+      borderStyle: "solid",
+      marginVertical: RFValue(8),
+      padding: RFValue(8),
+      width: "88%",
     },
   });
 
