4a51d0137ffe5f4ea12c5e134d4f274efe78f92c
TeqApp <teqa@wp.pl>
2022-04-15 19:57:31 +0200

    Added cartons reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import cartonsReducer from '../features/cartonsSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cartons: cartonsReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
